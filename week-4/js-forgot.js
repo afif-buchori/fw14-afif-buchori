468b5b8e8b0b0727e3a399090e2e79a4066ab61f
afif buchori <4fifbuchori@gmail.com>
2023-02-27 19:29:59 +0700

    menambahkan DOM untuk validasi email

--- week-4/js-forgot.js
+++ week-4/js-forgot.js
@@ -1,9 +1,39 @@
-const btnForgot = document.getElementById('submit-forgot');
-btnForgot.addEventListener('click', function() {
-    const inputEmail = document.getElementById('email').value;
-    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputEmail)){
-        alert('Format Email Salah');
+const inputEmail = document.getElementById('email');
+
+const noteEmail = document.querySelector('form .input');
+noteEmail.style.position = 'relative';
+noteEmail.style.display = 'flex';
+
+const infoEmail = document.createElement('p');
+infoEmail.style.position = 'absolute';
+infoEmail.style.top = '6px';
+infoEmail.style.left = '16px';
+infoEmail.style.fontSize = '16px';
+
+inputEmail.addEventListener('input', function() {
+    noteEmail.appendChild(infoEmail);
+    if(inputEmail.value == '') {
+        infoEmail.style.color = 'black';
+        infoEmail.innerHTML = '*input kosong';
+        btnForgot.disabled = true;
+        btnForgot.style.cursor = 'default';
+    } 
+    else if(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputEmail.value)) {
+        infoEmail.style.color = 'green';
+        infoEmail.innerHTML = '*OK';
+        btnForgot.disabled = false;
+        btnForgot.style.cursor = 'pointer';
     } else {
-        console.log('Email :', inputEmail);
+        infoEmail.style.color = 'red';
+        infoEmail.innerHTML = '*Bukan Email';
+        btnForgot.disabled = true;
+        btnForgot.style.cursor = 'default';
     }
+});
+
+const btnForgot = document.getElementById('submit-forgot');
+btnForgot.disabled = true;
+btnForgot.style.cursor = 'default';
+btnForgot.addEventListener('click', function() {
+    console.log('Email :', inputEmail.value);
 });
